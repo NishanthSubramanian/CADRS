a30581fcbecd8cd13f5a27d50d7a497229c4726b
Utkarsh Patil <utkarsh2305@gmail.com>
2018-10-26 16:35:12 +0400

    Add drone algorithm illustration

--- drone_path_algorithm/js/index.js
+++ drone_path_algorithm/js/index.js
@@ -1,3 +1,46 @@
+var fetchIllustration;
+
+const createTable = (illustrations, state) => {
+	var stateIllustration = illustrations[state][0];
+	var tableElement = document.getElementById('state-table');
+	tableElement.innerHTML = `<tr>
+		<th colspan="5">State ${state}</th>
+	</tr>`;
+	for (var drone = 0; drone < Object.keys(stateIllustration).length; drone++) {
+		var droneState = stateIllustration[drone];
+		if (droneState['type'] === 'travelling') {
+			tableElement.innerHTML += `<tr>
+				<th id="table-drone-${drone}" rowspan="2">Drone ${drone}</th>
+				<th>Source Node</th>
+				<th>Destination Node</th>
+				<th>Distance from Source</th>
+				<th>Distance to Destination</th>
+			</tr>
+			<tr>
+				<td id="source-${drone}">${droneState['source']}</td>
+				<td id="dest-${drone}">${droneState['destination']}</td>
+				<td id="s-dist-${drone}">${droneState['sourceDist']}</td>
+				<td id="d-dist-${drone}">${droneState['destinationDist']}</td>
+			</tr>`;
+		} else if ((droneState['type'] === 'based') ||
+			(droneState['type'] === 'dead')) {
+			tableElement.innerHTML += `<tr>
+				<th id="table-drone-${drone}" rowspan="2">Drone ${drone}</th>
+				<th>Source Node</th>
+				<th>Destination Node</th>
+				<th>Distance from Source</th>
+				<th>Distance to Destination</th>
+			</tr>
+			<tr>
+				<td id="source-${drone}">&#8211;</td>
+				<td id="dest-${drone}">&#8211;</td>
+				<td id="s-dist-${drone}">&#8211;</td>
+				<td id="d-dist-${drone}">&#8211;</td>
+			</tr>`;
+		}
+	}
+}
+
 const main = () => {
 	var nodes = 9, drones = 3;
 	var distArray = {
@@ -67,9 +110,57 @@ const main = () => {
 		8: 15,
 	};
 	var dbpaInstance = new DBPA(nodes, drones, distArray, urgArray);
-	var displayElement = document.getElementById('content');
-	displayElement.innerHTML = JSON.stringify(dbpaInstance.runAlgorithm(), null, 4);
-}
+	var drone_path = dbpaInstance.runAlgorithm();
+	var svgNodes = {
+		0: [200, 280],
+		1: [340, 100],
+		2: [400, 250],
+		3: [380, 425],
+		4: [650, 450],
+		5: [575, 150],
+		6: [550, 300],
+		7: [800, 166],
+		8: [775, 375],
+	};
+	Array.prototype.contains = function(obj) {
+	    var i = this.length;
+	    while (i--) {
+	        if (this[i] == obj) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	// Begin Illustration
+	illustrations = getIllustrationStates(drone_path, distArray, svgNodes, nodes);
+	fetchIllustration = val => {
+		var getState = illustrations[0][val];
+		var getNodes = getState[1];
+		var getDroneState = getState[0];
+		for (var node = 0; node < nodes; node++) {
+			var nodeElement = document.getElementById(`node-${node}`);
+			if (getNodes.contains(node)) {
+				nodeElement.setAttribute('style', 'fill: black;');
+			} else {
+				nodeElement.setAttribute('style', 'fill: green;');
+			}
+		}
+		for (var drone = 0; drone < Object.keys(getDroneState).length; drone++) {
+			var droneElement = document.getElementById(`drone-${drone}`);
+			droneElement.setAttribute('style', `d: path("${getDroneState[drone]['path']}");`);
+		}
+		document.getElementById('input-value').innerHTML = `${val} / ${illustrations[1] - 1}`;
+		createTable(illustrations[0], val);
+	};
+	var svg = document.getElementById('svg-illustration');
+	for (var drone = 0; drone < drones; drone++) {
+		svg.innerHTML += `<path id="drone-${drone}"></path>`;
+	}
+	var input = document.getElementById('state-picker');
+	input.setAttribute('max', illustrations[1] - 1);
+	fetchIllustration(input.value);
+	createTable(illustrations[0], input.value);
+};
 
 if ((document.readyState === 'complete') ||
 	((document.readyState === 'loading') &&
